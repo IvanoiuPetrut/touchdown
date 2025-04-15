5c5df76f6b5cb732e74d960d7480318d4a6b3144
Petrut Ivanoiu <petrut.ivanoiu27@gmail.com>
2025-04-15 16:57:54 +0300

    feat: add load level functionality

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,6 +3,8 @@ extends Node
 # References
 @onready var player = %Player
 @onready var game_ui = %GameUi
+@onready var world: Node2D = %World2D
+
 
 # Game state
 var current_level = 1
@@ -10,6 +12,28 @@ var current_world = 1
 var high_score = 0
 
 func _ready():
+	# Load level 1 from world 1
+	#var level_path = "res://scenes/levels/world_1/level_2.tscn"
+	#var level_scene = load(level_path)
+	#var newLevel = level_scene.instantiate()
+	#world.add_child(newLevel)
+	_change_level(1, 2)
+	#var level_scene = load(level_path)
+	#if level_scene:
+		#var level_instance = level_scene.instantiate()
+		## Add the level to World2D (assuming it's a direct child of the game manager)
+		#var world_node = $World2D
+		#if world_node:
+			## Remove any existing levels
+			#for child in world_node.get_children():
+				#child.queue_free()
+			## Add the new level
+			#world_node.add_child(level_instance)
+		#else:
+			#push_error("World2D node not found. Make sure it's a direct child of the game manager.")
+	#else:
+		#push_error("Failed to load level: " + level_path)
+	
 	# Connect to player stats changed signal
 	player.stats_changed.connect(_update_ui)
 	
@@ -75,3 +99,12 @@ func _handle_crash():
 func _handle_successful_landing():
 	# Show success message via mission status
 	game_ui.show_message("SUCCESSFUL LANDING!")
+
+func _change_level(worldNumber: int, levelNumber: int):
+#	res://scenes/levels/world_1/level_1.tscn
+	const FILE_BEGIN = "res://scenes/levels/"
+	var level_path = FILE_BEGIN + "world_" + str(worldNumber) + "/level_" + str(levelNumber) + ".tscn"
+	var level_scene = load(level_path)
+	var newLevel = level_scene.instantiate()
+	world.add_child(newLevel)
+	pass
